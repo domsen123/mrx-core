8815f4940039c5570279c9e385e32ac5d1dce88e
MARX Dominic <dominic.marx@axians.de>
2022-03-18 09:27:34 +0100

    feat: client-settings

--- @core/client/src/App.vue
+++ @core/client/src/App.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
-v-app
-  v-main
-    h1 App.vue
-    router-view
+router-view   
 </template>
 
 <script lang="ts" setup></script>
--- @core/types/define.ts
+++ @core/types/define.ts
@@ -1,10 +1,13 @@
 import type { RouteRecordRaw } from './client';
 
+export interface ClientSettings
+  extends Record<string | number | symbol, unknown> {}
+
 export interface ClientDefinition {
   name: string;
   routes?: RouteRecordRaw[];
   plugins?: Promise<ClientDefinition>[];
-  settings?: any;
+  settings?: ClientSettings;
 }
 
 export interface PluginClientDefinition
